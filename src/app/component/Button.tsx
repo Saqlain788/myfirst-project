cf7f06cc66f0b6ebd2e7674ec854f1551f40aa0e
Saqlain788 <saqlain788@gmail.com>
2023-10-09 15:20:22 +0500

    updates

--- src/app/component/Button.tsx
+++ src/app/component/Button.tsx
@@ -1,6 +1,8 @@
 function Button() {
   return (
-    <button className="py-2 px-4 rounded-md bg-red-700">click me here</button>
+    <button className="py-2 px-4 rounded-md bg-red-700">
+      Allama Sb Button ko na kholo
+    </button>
   );
 }
 
